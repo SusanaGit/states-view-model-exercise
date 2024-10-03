b6d3333460fa633336eb740693b70998111bf347
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-03 14:41:53 +0200

    starting app

---
+++ app/src/main/java/com/susanafigueroa/states_viewmodelexercise/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.susanafigueroa.states_viewmodelexercise
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.susanafigueroa.states_viewmodelexercise.ui.theme.StatesViewModelExerciseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StatesViewModelExerciseTheme {
+                StatesViewModelExercise()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+}
+
+@Composable
+fun StatesViewModelExercise() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StatesViewModelExerciseTheme {
+        StatesViewModelExercise()
+    }
+}
